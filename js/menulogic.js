800316a18e32e766c0ad518921092543c4876446
Kieran Williams <79399574+kieranwilliamsdev@users.noreply.github.com>
2021-04-23 08:03:11 +0100

    menu construction

---
+++ js/menulogic.js
@@ -0,0 +1,6 @@
+var createmenu = document.createElement('div');
+var menuposition = document.getElementById('nav');
+
+createmenu.className = 'nav';
+createmenu.id = 'memuid';
+
--- js/toggle.js
+++ js/toggle.js
@@ -61,3 +61,5 @@ function reset_toggle() {
 }
 
 
+
+
