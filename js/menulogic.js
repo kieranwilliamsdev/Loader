23ae16c4403a90623051cfb4783300977ae2d4cb
kieranwilliamsdev <79399574+kieranwilliamsdev@users.noreply.github.com>
2021-04-24 09:41:59 +0100

    Update for menu
    Started implementing me logic

--- js/menulogic.js
+++ js/menulogic.js
@@ -1,6 +1,18 @@
-var createmenu = document.createElement('div');
-var menuposition = document.getElementById('nav');
+function openMenu() {
 
-createmenu.className = 'nav';
-createmenu.id = 'memuid';
+var createMenu = document.createElement('div');
+var firstdiv = document.getElementById('select_items');
 
+createMenu.className = 'nav';
+createMenu.id = 'memuid';
+
+document.body.insertBefore(createMenu, firstdiv);
+console.log('Div should be created here');
+}
+
+const menu_button = document.getElementById('burger_menu');
+
+menu_button.addEventListener('click', () => {
+    openMenu();
+    console.log('button Pressed');
+})
